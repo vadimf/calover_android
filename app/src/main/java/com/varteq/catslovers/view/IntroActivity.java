fa1c79295c14957addcae5d10b657b77e35ac92a
Artem <amakhovyk@varteq.com>
2017-11-13 14:38:19 +0200

    initial commit

---
+++ app/src/main/java/com/varteq/catslovers/view/IntroActivity.java
@@ -0,0 +1,184 @@
+package com.varteq.catslovers.view;
+
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.varteq.catslovers.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+
+    float initialX;
+    private ImageSwitcher myImageSwitcher;
+    private int counter = 0;
+    private int imageSwitcherImages[] = {R.drawable.cat1, R.drawable.cat2, R.drawable.cat3};
+    private MyViewPagerAdapter myViewPagerAdapter;
+    public ViewPager viewPager;
+    private List<View> layouts = new ArrayList<>();
+    private TextView[] dots;
+    private LinearLayout dotsLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        for (int i = 0; i < imageSwitcherImages.length; i++) {
+            ImageView imageView = new ImageView(getApplicationContext());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(
+                    ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT
+            ));
+            imageView.setImageResource(imageSwitcherImages[i]);
+            layouts.add(imageView);
+        }
+
+        dots = new TextView[imageSwitcherImages.length];
+        dotsLayout = (LinearLayout) findViewById(R.id.dots_layout);
+        for (int i = 0; i < imageSwitcherImages.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(30);
+            dots[i].setTextColor(getResources().getColor(android.R.color.darker_gray));
+            dotsLayout.addView(dots[i]);
+        }
+        dots[0].setTextColor(getResources().getColor(R.color.white));
+
+        // adding bottom dots
+        addBottomDots(0);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+        viewPager.setOffscreenPageLimit(2);
+    }
+
+    private void addBottomDots(int currentPage) {
+
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+            for (int i = 0; i < imageSwitcherImages.length; i++) {
+                dots[i].setTextColor(getResources().getColor(android.R.color.darker_gray));
+            }
+            dots[position].setTextColor(getResources().getColor(R.color.white));
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(layouts.get(position));
+
+            return layouts.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_intro);
+//
+//        myImageSwitcher = (ImageSwitcher) findViewById(R.id.intro_switcher);
+//
+//        myImageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+//            @Override
+//            public View makeView() {
+//                ImageView switcherImageView = new ImageView(getApplicationContext());
+//                switcherImageView.setLayoutParams(new ImageSwitcher.LayoutParams(
+//                        ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT
+//                ));
+//                switcherImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//                switcherImageView.setImageResource(R.drawable.cat1);
+//                return switcherImageView;
+//            }
+//        });
+//
+//        Animation animationOut = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+//        Animation animationIn = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+//
+//        myImageSwitcher.setOutAnimation(animationIn);
+//        myImageSwitcher.setInAnimation(animationIn);
+//    }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                initialX = event.getX();
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                float finalX = event.getX();
+//                if (initialX > finalX) {
+//                    counter++;
+//                    nextImage();
+//                }
+//                else if (counter > 0) {
+//                    counter--;
+//                    myImageSwitcher.showPrevious();
+//                }
+//                break;
+//        }
+//        return false;
+//    }
+//
+//    public void nextImage() {
+//        if (counter >= imageCount)
+//            counter = 0;
+//        if (counter < 0)
+//            counter = 0;
+//        myImageSwitcher.setImageResource(imageSwitcherImages[counter]);
+//    }
+}
