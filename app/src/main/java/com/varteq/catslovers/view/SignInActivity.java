6d7d7cad95384ec7cab998dd6a4ecd0d9ee86f71
viktord1985 <viktor.derk1985@gmail.com>
2017-12-14 13:39:09 +0200

    SignIn added, required get token request

--- app/src/main/java/com/varteq/catslovers/view/SignInActivity.java
+++ app/src/main/java/com/varteq/catslovers/view/SignInActivity.java
@@ -9,12 +9,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserCodeDeliveryDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.NewPasswordContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler;
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.varteq.catslovers.R;
 import com.varteq.catslovers.utils.Log;
 import com.varteq.catslovers.utils.Profile;
 import com.varteq.catslovers.utils.Toaster;
+import com.varteq.catslovers.utils.qb.CognitoAuthHelper;
+import com.varteq.catslovers.view.presenter.AuthPresenter;
+
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,11 +60,68 @@ public class SignInActivity extends BaseActivity {
         return findViewById(R.id.layout_root);
     }
 
+    private String username;
+    private String password;
+
     @OnClick(R.id.btnSignIn)
     void btnSignIn() {
         Log.d(TAG, "btnSignIn");
-        Toaster.shortToast(R.string.ComingSoon);
+        username = usernameEditText.getText().toString().trim();
+        password = passwordEditText.getText().toString().trim();
+        if (username.isEmpty()) {
+            Toaster.shortToast(R.string.UsernameIsEmpty);
+            return;
+        }
+        if (password.isEmpty()) {
+            Toaster.shortToast(R.string.PasswordIsEmpty);
+            return;
+        }
+        showWaitDialog();
+        CognitoAuthHelper.getPool().getUser(username).getSessionInBackground(authenticationHandler);
     }
+
+    AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+        @Override
+        public void onSuccess(CognitoUserSession cognitoUserSession, CognitoDevice device) {
+            Log.d(TAG, "AuthenticationHandler onSuccess");
+            hideWaitDialog();
+            CognitoAuthHelper.setCurrSession(cognitoUserSession);
+
+            //TODO call getToken and run MainActivity
+            Toaster.shortToast(R.string.ComingSoon);
+        }
+
+        @Override
+        public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String username) {
+            Log.d(TAG, "AuthenticationHandler getAuthenticationDetails");
+            hideWaitDialog();
+            if (username != null) {
+                CognitoAuthHelper.setUser(username);
+            }
+            AuthenticationDetails authenticationDetails = new AuthenticationDetails(username, password, null);
+            authenticationContinuation.setAuthenticationDetails(authenticationDetails);
+            authenticationContinuation.continueTask();
+        }
+
+        @Override
+        public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+            Log.d(TAG, "AuthenticationHandler getMFACode");
+            hideWaitDialog();
+        }
+
+        @Override
+        public void authenticationChallenge(ChallengeContinuation continuation) {
+            Log.d(TAG, "AuthenticationHandler authenticationChallenge");
+            hideWaitDialog();
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            Log.d(TAG, "AuthenticationHandler onFailure");
+            hideWaitDialog();
+            Toaster.shortToast(CognitoAuthHelper.formatException(e));
+        }
+    };
 
     @OnClick(R.id.btnSignUp)
     void btnSignUp() {
