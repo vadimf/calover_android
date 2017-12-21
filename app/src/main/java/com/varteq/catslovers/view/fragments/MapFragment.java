24a3d8e18af78d8cd6752aa3a659ea67f69562a0
Artem <amakhovyk@varteq.com>
2017-12-21 15:29:59 +0200

    Fixed issue with compass on maps

--- app/src/main/java/com/varteq/catslovers/view/fragments/MapFragment.java
+++ app/src/main/java/com/varteq/catslovers/view/fragments/MapFragment.java
@@ -191,7 +191,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(userLocation)      // Sets the center of the map to Mountain View
                 .zoom(17)                   // Sets the zoom
-                .bearing(90)                // Sets the orientation of the camera to east
+                .bearing(0)                // Sets the orientation of the camera to east
                 .tilt(30)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
         this.googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
