2bc3633fdb2f6fa8f6da271df96a1ce892201e04
Artem <amakhovyk@varteq.com>
2018-02-06 15:16:29 +0200

    #124: - "Age-description" block background added - text color in "Age-description" block changed

--- app/src/main/java/com/varteq/catslovers/view/presenter/AuthPresenter.java
+++ app/src/main/java/com/varteq/catslovers/view/presenter/AuthPresenter.java
@@ -100,6 +100,8 @@ public class AuthPresenter {
     private boolean isTestUser() {
         testUsers = new HashMap<>();
         testUsers.put("+380938315207", "09g@I9O%9!0?M!2l");
+        testUsers.put("+380638575894", "y4,2UqGY$0R5@dTE");
+        testUsers.put("+380933650962", "@a]F56GyiT!2X&y*");
         //testUsers.put("+380638773851", "CatTest123_");
         for (Map.Entry<String, String> entry : testUsers.entrySet()) {
             if (entry.getKey().equals(this.username)) {
