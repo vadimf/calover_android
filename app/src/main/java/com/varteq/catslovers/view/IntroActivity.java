eebae971c44bf3deaa5c1a4ab6e0625867ce92e6
Viktor Soltis <vsoltis@varteq.com>
2018-02-13 19:44:04 +0200

    119 [UI Refactoring] Match Onboarding to Reference
    - refactored IntroActivity class
    - removed unused code
    - tweaked access levels

--- app/src/main/java/com/varteq/catslovers/view/IntroActivity.java
+++ app/src/main/java/com/varteq/catslovers/view/IntroActivity.java
@@ -10,7 +10,6 @@ import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageSwitcher;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -25,19 +24,26 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+/**
+ * Activity class for greeting user logged for first-time.
+ */
 public class IntroActivity extends AppCompatActivity {
 
-    private String TAG = IntroActivity.class.getSimpleName();
-    float initialX;
-    private ImageSwitcher myImageSwitcher;
-    private int counter = 0;
-    private int imageSwitcherImages[] = {R.drawable.illustration_onbording_1, R.drawable.illustration_onboarding_2, R.drawable.onboarding_illustration_3};
-    private int imageSwitcherTexts[] = {R.string.onboarding_text_1, R.string.onboarding_text_2, R.string.onboarding_text_3};
-    private MyViewPagerAdapter myViewPagerAdapter;
+    private static final String TAG = IntroActivity.class.getSimpleName();
+
+    private final int imageSwitcherImages[] = {R.drawable.illustration_onbording_1,
+            R.drawable.illustration_onboarding_2,
+            R.drawable.onboarding_illustration_3};
+
+    private final int onboardingLayouts[] = {R.layout.onbording_layout_1,
+            R.layout.onbording_layout_2,
+            R.layout.onbording_layout_3};
+
+    private List<View> layouts;
+    private TextView[] dots;
+
     @BindView(R.id.view_pager)
     ViewPager viewPager;
-    private List<View> layouts = new ArrayList<>();
-    private TextView[] dots;
     @BindView(R.id.dots_layout)
     LinearLayout dotsLayout;
     @BindView(R.id.go_to_login_button)
@@ -55,43 +61,6 @@ public class IntroActivity extends AppCompatActivity {
         initUI();
     }
 
-    private void initUI() {
-        for (int i = 0; i < imageSwitcherImages.length; i++) {
-            int resId = R.layout.onbording_layout_1;
-            if (i == 1)
-                resId = R.layout.onbording_layout_2;
-            else if (i == 2)
-                resId = R.layout.onbording_layout_3;
-
-            View layout = LayoutInflater.from(getApplicationContext())
-                    .inflate(resId, null, false);
-
-            scaleOnboardingView(layout);
-
-            layouts.add(layout);
-        }
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.rightMargin = Utils.convertDpToPx(23, this);
-        dots = new TextView[imageSwitcherImages.length];
-        for (int i = 0; i < imageSwitcherImages.length; i++) {
-            dots[i] = new TextView(this);
-            if (i < imageSwitcherImages.length - 1)
-                dots[i].setLayoutParams(layoutParams);
-            dots[i].setText(Html.fromHtml("&#8226;"));
-            dots[i].setTextSize(40);
-            dotsLayout.addView(dots[i]);
-        }
-
-        selectBottomDot(0);
-
-        myViewPagerAdapter = new MyViewPagerAdapter();
-        viewPager.setAdapter(myViewPagerAdapter);
-        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
-        viewPager.setOffscreenPageLimit(2);
-        viewPager.setClipToPadding(false);
-    }
-
     @OnClick({R.id.go_to_login_button, R.id.start_button})
     void goToLogin() {
         Log.d(TAG, "goToLogin");
@@ -100,45 +69,101 @@ public class IntroActivity extends AppCompatActivity {
         startActivity(new Intent(this, SignUpActivity.class));
     }
 
+    private void initUI() {
+
+        initializeOnboardingLayouts();
+
+        initializeDots();
+
+        initializeViewPager();
+    }
+
+    private void initializeOnboardingLayouts() {
+
+        layouts = new ArrayList<>();
+
+        for (int i = 0; i < onboardingLayouts.length; i++) {
+            int resId = onboardingLayouts[i];
+
+            View layout = LayoutInflater.from(getApplicationContext())
+                    .inflate(resId, null, false);
+
+            scaleOnboardingView(layout);
+
+            layouts.add(layout);
+        }
+    }
+
+    private void initializeDots() {
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.rightMargin = Utils.convertDpToPx(23, this);
+        dots = new TextView[imageSwitcherImages.length];
+        for (int i = 0; i < imageSwitcherImages.length; i++) {
+            dots[i] = new TextView(this);
+            if (i < imageSwitcherImages.length - 1) {
+                dots[i].setLayoutParams(layoutParams);
+            }
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(40);
+            dotsLayout.addView(dots[i]);
+        }
+
+        selectBottomDot(0);
+    }
+
+    private void initializeViewPager() {
+
+        viewPager.setAdapter(new IntroPagerAdapter());
+        viewPager.addOnPageChangeListener(new IntroPageChangeListener());
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setClipToPadding(false);
+    }
+
+    /**
+     * Scale onboarding view for high resolution displays.
+     *
+     * @param view View
+     */
+    private void scaleOnboardingView(View view) {
+
+        final float PIXEL_DISPLAY_WIDTH_DP = 360.0f;
+        final float PIXEL_DISPLAY_HEIGHT_DP = 640.0f;
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+
+        int displayWidth = displayMetrics.widthPixels;
+        int displayHeight = displayMetrics.heightPixels;
+
+        float displayDensity = displayMetrics.density;
+
+        int displayWidthInDp = (int) (displayWidth / displayDensity);
+        int displayHeightInDp = (int) (displayHeight / displayDensity);
+
+        // Compare it with Pixel sizes (1080 x 1920, density - 3.0)
+        if (displayWidthInDp > PIXEL_DISPLAY_WIDTH_DP || displayHeightInDp > PIXEL_DISPLAY_HEIGHT_DP) {
+            float factorW = displayWidthInDp / PIXEL_DISPLAY_WIDTH_DP;
+            float factorH = displayHeightInDp / PIXEL_DISPLAY_HEIGHT_DP;
+
+            // Get lower factor
+            float factor = factorW < factorH ? factorW : factorH;
+
+            // Scale onboarding view
+            view.setScaleX(factor);
+            view.setScaleY(factor);
+            view.requestLayout();
+        }
+    }
+
     private void selectBottomDot(int currentPage) {
         for (int i = 0; i < imageSwitcherImages.length; i++) {
             dots[i].setTextColor(getResources().getColor(R.color.colorPrimary));
         }
+
         dots[currentPage].setTextColor(getResources().getColor(R.color.white));
     }
 
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-
-        @Override
-        public void onPageSelected(int position) {
-            selectBottomDot(position);
-            if (position == imageSwitcherImages.length - 1) {
-                dotsLayout.setVisibility(View.INVISIBLE);
-                goToLoginButton.setVisibility(View.INVISIBLE);
-                startButton.setVisibility(View.VISIBLE);
-            } else {
-                dotsLayout.setVisibility(View.VISIBLE);
-                goToLoginButton.setVisibility(View.VISIBLE);
-                startButton.setVisibility(View.INVISIBLE);
-            }
-        }
-
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-
-        }
-    };
-
-
-    public class MyViewPagerAdapter extends PagerAdapter {
-
-        public MyViewPagerAdapter() {
-        }
+    private class IntroPagerAdapter extends PagerAdapter {
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
@@ -164,35 +189,30 @@ public class IntroActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Scale onboarding view for high resolution displays.
-     *
-     * @param view View
-     */
-    private void scaleOnboardingView(View view) {
+    private class IntroPageChangeListener implements ViewPager.OnPageChangeListener {
 
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        @Override
+        public void onPageSelected(int position) {
+            selectBottomDot(position);
+            if (position == imageSwitcherImages.length - 1) {
+                dotsLayout.setVisibility(View.INVISIBLE);
+                goToLoginButton.setVisibility(View.INVISIBLE);
+                startButton.setVisibility(View.VISIBLE);
+            } else {
+                dotsLayout.setVisibility(View.VISIBLE);
+                goToLoginButton.setVisibility(View.VISIBLE);
+                startButton.setVisibility(View.INVISIBLE);
+            }
+        }
 
-        int displayWidth = displayMetrics.widthPixels;
-        int displayHeight = displayMetrics.heightPixels;
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+            // Do nothing
+        }
 
-        float displayDensity = displayMetrics.density;
-
-        int displayWidthInDp = (int) (displayWidth / displayDensity);
-        int displayHeightInDp = (int) (displayHeight / displayDensity);
-
-        // Compare it with Pixel sizes (1080 x 1920, density - 3.0)
-        if (displayWidthInDp > 360 || displayHeightInDp > 640) {
-            float factorW = displayWidthInDp / 360.0f;
-            float factorH = displayHeightInDp / 640.0f;
-
-            // Get lower factor
-            float factor = factorW < factorH ? factorW : factorH;
-
-            // Scale onboarding view
-            view.setScaleX(factor);
-            view.setScaleY(factor);
-            view.requestLayout();
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+            // Do nothing
         }
     }
 }
